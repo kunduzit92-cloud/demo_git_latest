1db8338d6c5d6d0113c35f475306e36db2f75c62
kunduz <kunduz.it92@gmail.com>
2026-05-31 18:42:11 -0400

    second commit

--- src/test/java/qa/testcases/NewTestCaseForGitDemo.java
+++ src/test/java/qa/testcases/NewTestCaseForGitDemo.java
@@ -5,6 +5,9 @@ public class NewTestCaseForGitDemo {
     public static void main(String[] args) {
         System.out.println(" new test to push to repo");
         System.out.println("  testgit status");
+        System.out.println();
+        System.out.println("new code");
+        System.out.println("123");
     }
 }
 
